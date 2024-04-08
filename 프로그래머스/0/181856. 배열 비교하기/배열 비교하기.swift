6119b75903185a2a32d9c6bbd7a8c44929ea890d
ciu <a2849535@gmail.com>
2024-04-08 23:45:18 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ arr1: [Int], _ arr2: [Int]) -> Int {
+    if arr1.count != arr2.count {
+        return arr1.count > arr2.count ? 1 : -1
+    } else {
+        let sum1 = arr1.reduce(0, +)
+        let sum2 = arr2.reduce(0, +)
+        
+        if sum1 == sum2 {
+            return 0
+        } else {
+            return sum1 > sum2 ? 1 : -1
+        }
+    }
+}
