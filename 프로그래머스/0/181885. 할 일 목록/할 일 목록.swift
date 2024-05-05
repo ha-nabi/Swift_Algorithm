f6392038f9be225e7e43873355d7aecb68e0462f
ciu <a2849535@gmail.com>
2024-05-06 02:10:01 +0900

    [level 0] Title: 할 일 목록, Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ todo_list:[String], _ finished:[Bool]) -> [String] {
+    var result: [String] = []
+    
+    for (index, isFinished) in finished.enumerated() {
+        if !isFinished {
+            result.append(todo_list[index])
+        }
+    }
+    
+    return result
+}
