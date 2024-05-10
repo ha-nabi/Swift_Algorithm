f8db4071258637cc8dffe768c3254e77de413f02
ciu <a2849535@gmail.com>
2024-05-10 16:20:28 +0900

    [level 0] Title: 5명씩, Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ names:[String]) -> [String] {
+    var result: [String] = []
+    
+    for index in stride(from: 0, to: names.count, by: 5) {
+        result.append(names[index])
+    }
+    
+    return result
+}
