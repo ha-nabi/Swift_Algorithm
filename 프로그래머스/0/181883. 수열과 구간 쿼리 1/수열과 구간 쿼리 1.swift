28021f119f97f8670fe90e3ec273e5a01af418bf
ciu <a2849535@gmail.com>
2024-07-11 14:29:14 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.75 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
+    var result = arr
+    
+    for query in queries {
+        let s = query[0]
+        let e = query[1]
+        for i in s...e {
+            result[i] += 1
+        }
+    }
+    
+    return result
+}
