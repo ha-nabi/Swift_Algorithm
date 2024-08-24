8c0b7efb2d823107a8bdb6b952cf3acc986a8c65
ciu <a2849535@gmail.com>
2024-08-25 00:04:18 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.07 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ n:Int) -> [[Int]] {
+    var arr = Array(repeating: Array(repeating: 0, count: n), count: n)
+    
+    for i in 0..<n {
+        arr[i][i] = 1
+    }
+    
+    return arr
+}
